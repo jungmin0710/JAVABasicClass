9a5b666718ac9aea67758a2188b782ce722fab02
jungmin0710 <67640843+jungmin0710@users.noreply.github.com>
2020-10-21 12:04:04 +0900

    Create AdvancedWhile.java

---
+++ AdvancedWhile.java
@@ -0,0 +1,18 @@
+//서로 다른 세 정수를 만들자
+//Math.random 사용
+class Main {
+  public static void main(String[] args) {
+    int range = 10;
+    int start = 1;
+    int a = 0, b = 0, c = 0;
+
+    while(true) {
+      a = (int)(range*Math.random()+start);
+      b = (int)(range*Math.random()+start);
+      c = (int)(range*Math.random()+start);
+      if (a != b && b != c && c != a) { break;}
+    }
+
+    System.out.printf("%d\t%d\t%d",a,b,c);
+  }
+}
