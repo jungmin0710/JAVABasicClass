f3880f21cf95954f8d11f18ad53f3233f927878b
jungmin0710 <67640843+jungmin0710@users.noreply.github.com>
2020-10-27 11:56:47 +0900

    Update DrawRhombus.java

--- DrawRhombus.java
+++ DrawRhombus.java
@@ -3,10 +3,11 @@ for에 for문에서는(2중첩) 바깥 for문의 스텝이 연결될 때마다 �
 바깥 for문 n줄에 대해서 내부 for문을 실행한다. i가 0일때 안쪽 for문의 j < 0가 되므로 공백이 없다.
 */
 
-//방법 1: 쉽게 그리는 방법
+
 class Main {
   //마름모 그리기
-  public static void Rhombus1(int n){
+  //방법 1
+  public static void rhombus1(int n){
     //상단: 정삼각형
     for (int i = 0; i < n; i++){
       for (int j = n-i-1; j > 0; j--){
@@ -29,9 +30,24 @@ class Main {
     }
   }
 
+  //방법 2
+  public static void rhombus2(int n){
+    for (int i = 0; i < n; i++){
+      //모양을 잡아줄 공백
+      for (int j = 0; j < Math.abs(i - n/2); j++){
+        System.out.print(" ");
+      }
+      //별 표시
+      for (int k = 0; k < n-2*Math.abs(i-n/2)-1; k++){
+        System.out.print("*");
+      }
+      System.out.println();
+
+    }
+  }
 
   //메인메서드
   public static void main(String[] args) {
-    Rhombus1(10);//층 수
+    rhombus2(10);
   }
 }
