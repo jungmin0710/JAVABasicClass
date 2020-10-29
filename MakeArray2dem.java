43c672ad481ffe4327cc2bbfa0fea929c4748a7a
jungmin0710 <67640843+jungmin0710@users.noreply.github.com>
2020-10-29 11:49:23 +0900

    Create MakeArray2dem.java

---
+++ MakeArray2dem.java
@@ -0,0 +1,40 @@
+//100이하의 자연수를 입력받아 그 숫자의 크기만큼 2차원 배열로 만들고 아래와 같이 출력하시오
+
+/*
+입력 : 5
+
+1 2 3 4 5
+2 4 6 8 10
+3 6 9 12 15
+4 8 12 16 20
+5 10 15 20 25
+
+*/
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner stdIn = new Scanner(System.in);
+    int n;
+    do{
+      n = stdIn.nextInt();
+      if (n > 100) System.out.println("다시 입력해주세요.");
+    }while (n > 100);
+
+    int[][] arr = new int[n][n];
+
+    for (int i = 0; i < n; i++){
+      for (int j = 0; j < n; j++){
+        arr[i][j] = (i+1)*(j+1);
+      }
+    }
+
+    for (int i = 0; i < n; i++){
+      for (int j = 0; j < n; j++){
+        System.out.print(arr[i][j] + " ");
+      }
+      System.out.println();
+    }
+   }
+}
